fc8099405f1190a424071c3514d319cd810b44c1
Mieoww <hari.mirasoa@gmail.com>
2025-01-11 22:20:07 +0530

    brute force

--- src/caesarCipher.java
+++ src/caesarCipher.java
@@ -4,7 +4,7 @@ public class caesarCipher {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-
+        //Encryption----------------------------------------
         System.out.println("Enter a text : ");
         String text = sc.nextLine();
 
@@ -15,16 +15,21 @@ public class caesarCipher {
         String encryptedText = encrypt(text, key);
 
         System.out.println("Encrypted text : "+encryptedText);
-
+        //Decryption-------------------------------------------
         System.out.println("Enter an encrypted Text : ");
         String a = sc.nextLine();
 
         System.out.println("Eter the key : ");
         int k = sc.nextInt();
+        sc.nextLine();
 
         String plaintext = decrypt(a, k);
 
         System.out.println("Plain text : "+plaintext);
+        //Brute force-----------------------------------------------
+        System.out.println("Enter an encrypted Text : ");
+        text = sc.nextLine();
+        bruteForce(text);
 
     }
     public static String encrypt(String text, int key)
@@ -63,6 +68,15 @@ public class caesarCipher {
 
         }
         return new String(plaintext);
+    }
+    public static void bruteForce(String text)
+    {
+        System.out.println("Possible option : ");
+        for(int i=1; i<=26; i++)
+        {
+            String option = decrypt(text, i);
+            System.out.println("Key : "+i+" Text : "+option);
+        }
     }
 }
 
